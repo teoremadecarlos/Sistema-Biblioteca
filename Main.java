496b816056f5600cd2fa90135ba059ba5cb91f85
Carlos João <119708348+teoremadecarlos@users.noreply.github.com>
2024-05-28 19:14:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int opcaoMenu = 99;
+        int opcaoMenu = -1;
 
-        while (opcaoMenu != 0) {
+        while (opcaoMenu < 0) {
             System.out.println("Olá, bem vindo a Biblioteca Uninassau!");
             System.out.println("Escolha uma das opções: ");
-
             System.out.println("1 - Usuário ");
             System.out.println("2 - Livro ");
             System.out.println("3 - Empréstimo ");
@@ -16,46 +15,49 @@ public class Main {
 
             System.out.print("Digite a opção: ");
             opcaoMenu = scanner.nextInt();
-        
 
-        switch (opcaoMenu) {
-            case 1:
-                System.out.println("Opção 1 selecionada!");
-
-                System.out.println("Aqui está o menu: ");
-
-                System.out.println("1 - Cadastrar Usuário ");
-                System.out.println("2 - Buscar Usuário ");
-                System.out.println("3 - Listar Usuário ");
-                System.out.println("4 - Deletar Usuário ");
-                System.out.println("0 - Voltar");
-
-                break;
-            case 2:
-                System.out.println("Opção 2 selecionada!");
-
-                System.out.println("Aqui está o menu: ");
-
-                System.out.println("1 - Cadastrar Livro");
-                System.out.println("2 - Buscar Livro por Título");
-                System.out.println("3 - Listar Livro");
-                System.out.println("4 - Deletar Livro");
-                System.out.println("0 - Voltar");
-
-                System.out.print("Escolha a opção: ");
-
-                break;
-            case 3:
-
-                break;
-            case 0:
-                System.out.println("Adeus, até a próxima!");
-                break;
-            default:
-                System.out.println("Opção Inválida!");
-                break;
+            switch (opcaoMenu) {
+                case 1:
+                    System.out.println("\nOpção 1 selecionada!");
+                    System.out.println("Aqui está o menu usuário: ");
+                    System.out.println("1 - Cadastrar Usuário ");
+                    System.out.println("2 - Buscar Usuário ");
+                    System.out.println("3 - Listar Usuário ");
+                    System.out.println("4 - Deletar Usuário ");
+                    System.out.println("0 - Voltar");
+                    System.out.print("Escolha a opção: ");
+                    int menuUsuario = scanner.nextInt();
+                    break;
+                case 2:
+                    System.out.println("\nOpção 2 selecionada!");
+                    System.out.println("Aqui está o menu livro: ");
+                    System.out.println("1 - Cadastrar Livro");
+                    System.out.println("2 - Buscar Livro por Título");
+                    System.out.println("3 - Listar Livro");
+                    System.out.println("4 - Deletar Livro");
+                    System.out.println("0 - Voltar");
+                    System.out.print("Escolha a opção: ");
+                    int menuLivro = scanner.nextInt();
+                    break;
+                case 3:
+                    System.out.println("\nOpção 3 selecionada!");
+                    System.out.println("Aqui está o menu empréstimo: ");
+                    System.out.println("1 - Criar Empréstimo ");
+                    System.out.println("2 - Buscar Empréstimo ");
+                    System.out.println("3 - Listar Emprestimos ");
+                    System.out.println("4 - Deletar Empréstimo ");
+                    System.out.println("0 - Voltar");
+                    System.out.print("Escolha a opção: ");
+                    int menuEmprestimo = scanner.nextInt();
+                    break;
+                case 0:
+                    System.out.println("\nAdeus, até a próxima!");
+                    break;
+                default:
+                    System.out.println("Opção Inválida!");
+                    break;
+            }
         }
         scanner.close();
-    }
     }
 }
